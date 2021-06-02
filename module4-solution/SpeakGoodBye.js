c1dfc31dc21b6ccbadc8bcb986420d3d28cfb1dc
Viditpatni <18.vidit@gmail.com>
2021-06-02 10:45:24 +0530

    html,js

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function(){
+    var byeSpeaker ={};
+var speakWord = "Good Bye";
+byeSpeaker.speak=function speak(name) {
+  console.log(speakWord + " " + name);
+}
+window.byeSpeaker=byeSpeaker;
+})();
---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,8 @@
+(function(){
+var helloSpeaker={};
+var speakWord = "Hello";
+helloSpeaker.speak = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+window.helloSpeaker=helloSpeaker;
+})();
